dfb8aaa6495dff466c3c1e7fb773aca6939c2970
SnowAqua55 <63825945+SnowAqua55@users.noreply.github.com>
2025-06-20 20:23:16 +0900

    🎉 add : Map Script
    맵 스크립트 제작 준비

---
+++ Assets/01.Scripts/Map/MapDataBase.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MapDataBase : MonoBehaviour
+{
+	
+	
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+	
+	
+}
