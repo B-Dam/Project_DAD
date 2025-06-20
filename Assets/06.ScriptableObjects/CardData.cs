e117c4bc6f3c0376658809bb2186c3904ccc4418
송정훈 <206493355+B-Dam@users.noreply.github.com>
2025-06-20 15:09:00 +0900

    🎉add : DoTween 패키지 설치, CardData.cs 추가 외
    카드 애니메이션 용 DoTween 패키지를 설치했습니다.
    스킬 카드 DB를 담을 SO 스크립트를 작성했습니다.
    skillcardDB.csv 파일을 가져왔습니다.

---
+++ Assets/06.ScriptableObjects/CardData.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public enum OwnerType { Player, CaptainCat } // 차후 보스가 추가될 때 추가
+public enum CardTypePrimary { Attack, Debuff, Defense }
+public enum CardTypeSecondary { None, Attack, Debuff, Defense }
+
+[CreateAssetMenu(menuName = "Battle/CardData")]
+public class CardData : ScriptableObject
+{
+    [Header("매핑된 키값")]
+    public string cardId;              // 예: "SC001"
+    public string displayName;         // 예: "물기"
+
+    [Header("소유자 타입")]
+    public OwnerType ownerType;           // "player", "captainCat" 등
+
+    [Header("카드 분류")]
+    public CardTypePrimary typePrimary;         // "공격", "약화", "방어"
+    public CardTypeSecondary typeSecondary;       // 보조 분류
+
+    [Header("효과 수치")]
+    public int effectAttackValue;      // 공격 계수
+    public int effectDefenseValue;     // 방어 계수
+    public int effectDebuffValue;      // 디버프 계수
+    public int effectTurnValue;        // 지속 턴 수
+
+    [TextArea(2,4), Header("효과 텍스트")]
+    public string effectText;          // 효과 설명
+
+    [Header("등급")]
+    public int rank;                   // 카드의 등급 (1성=1, 2성=2)
+    
+    [Header("애니메이션")]
+    public string animationID;         // 재생할 애니메이션 트리거
+    
+    [Header("소모 AP")]
+    public int costAP;                 // 기본적으로는 1만을 소모하지만 확장성을 위해 추가
+    
+    [Header("UI 리소스")]
+    public Sprite icon; 
+}
