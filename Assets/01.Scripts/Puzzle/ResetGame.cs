f6018e64efa65520f1e6d2622498d26d6e442883
YuJHun <76084438+YuJHun@users.noreply.github.com>
2025-06-24 19:38:16 +0900

    fix : 키보드 R키 눌러서 리셋가능

--- Assets/01.Scripts/Puzzle/ResetGame.cs
+++ Assets/01.Scripts/Puzzle/ResetGame.cs
@@ -17,9 +17,19 @@ public class ResetGame : MonoBehaviour
             Debug.LogWarning("Reset 버튼이 연결되지 않았습니다.");
         }
     }
+    void Update()
+    {
+        // 키보드 R 키로도 리셋
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RestartScene();
+        }
+    }
     public void RestartScene()
     {
         Time.timeScale = 1f; // 혹시 멈춘 상태였으면 다시 정상 속도로
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+   
+
 }
