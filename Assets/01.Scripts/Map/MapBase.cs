e4f81194f145afa3375646f9c3c88d01cab35c7c
MGBurtonKim <210243263+GMBurtonKim@users.noreply.github.com>
2025-07-01 14:27:02 +0900

    🐛 bugfix: MapBase error fixed
    - MapBase error fixed
    - Delected Hierachy Things

--- Assets/01.Scripts/Map/MapBase.cs
+++ Assets/01.Scripts/Map/MapBase.cs
@@ -35,7 +35,7 @@ public abstract class MapBase : MonoBehaviour
 
     protected abstract void OnLoadMap();  // 맵을 호출할 때 작동하는 로직
 
-    public abstract void OnReleaseMap();  // 맵이 나갈 때 작동하는 로직
+    protected abstract void OnReleaseMap();  // 맵이 나갈 때 작동하는 로직
 
     protected void SpawnPointSet()
     {
